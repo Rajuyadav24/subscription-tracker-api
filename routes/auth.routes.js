679617127bc9d4409ae821c79e97bfa4923914ba
Raju yadav <kunapulirajuyadav@gmail.com>
2025-02-18 17:26:49 +0530

    AUthentication done (signup and signin end points)

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
+import { SignIn, SignOut, SignUp } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req,res) => res.send({ title : 'Sign Up'})); 
-authRouter.post('/sign-in', (req,res) => res.send({ title : 'Sign In'})); 
-authRouter.post('/sign-out', (req,res) => res.send({ title : 'Sign Out'})); 
+//path: /api/v1/auth/signup
+authRouter.post('/sign-up', SignUp); 
+authRouter.post('/sign-in', SignIn); 
+authRouter.post('/sign-out', SignOut); 
 
 export default authRouter;
