56fc62a7760392e6d5494aa3dba129dc29ad4acf
Raju yadav <kunapulirajuyadav@gmail.com>
2025-02-18 19:15:14 +0530

    added authorization, arcjet extra layer protection

--- config/env.js
+++ config/env.js
@@ -5,7 +5,8 @@ config({ path : `.env.${process.env.NODE_ENV || 'development'}.local` });
 // eslint-disable-next-line no-undef
 export const { PORT,NODE_ENV,
      DB_URI,
-     JWT_SECRET, JWT_EXPIRES_IN
+     JWT_SECRET, JWT_EXPIRES_IN,
+     ARCJET_KEY, ARCJET_ENV,
  } = process.env;
 
  
